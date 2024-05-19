b95d03a594d3d8626ee17434acda812ae4a36bbb
Shammika Wijesinghe <102298368+shux360@users.noreply.github.com>
2024-05-19 14:20:00 +0530

    first commit

---
+++ src/test/java/com/saw/photozclone/PhotozCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saw.photozclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotozCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
